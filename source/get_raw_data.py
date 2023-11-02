82382bb529d03d9f05fc6ef8a10dd9b9b68009c6
leomaurodesenv <leo.mauro.desenv@gmail.com>
2023-11-02 18:11:49 -0300

    feat: init luigi, add extract raw data

---
+++ source/get_raw_data.py
@@ -0,0 +1,20 @@
+import os
+import luigi
+import zipfile
+
+class ExtractRawData(luigi.Task):
+    data_path = luigi.Parameter(default="../data/sentiment-analysis-on-movie-reviews.zip")
+
+    def output(self):
+        return {
+            "test": luigi.LocalTarget('../data/output/test.tsv.zip'),
+            "train": luigi.LocalTarget('../data/output/train.tsv.zip'),
+        }
+
+    def run(self):
+        # Check if data file exists
+        assert os.path.exists(self.data_path)
+
+        # Unzip data file
+        with zipfile.ZipFile(self.data_path, 'r') as zip_ref:
+            zip_ref.extractall("../data/output/")
